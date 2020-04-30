0bbaf698e05f54b713574742386ba3d44f21d6c9
LazyJackyboy <skrok.jacek21@gmail.com>
2020-04-30 11:25:18 +0200

    REST API, build stage 4
    Edited additional 4 User Stories

--- src/main/java/pl/wsb/students/api/consts/ApiEndpoints.java
+++ src/main/java/pl/wsb/students/api/consts/ApiEndpoints.java
@@ -8,11 +8,12 @@ public class ApiEndpoints {
     public static final String MOVIE = "/movie";
     public static final String USER = "/user";
 
+    public static final String USER_LOGOUT = "/logout";
     public static final String MOVIE_RATE = "/rate";
-    public static final String MOVIE_ID_ACCEPT = ApiEndpoints.QUERY_PARAM_ID + "/accept";
-    public static final String MOVIE_ID_REJECT = ApiEndpoints.QUERY_PARAM_ID + "/reject";
     public static final String MOVIE_COMMENT = "/comment";
     public static final String MOVIE_ACTOR_RATE = ApiEndpoints.QUERY_PARAM_ID +"/rate";
+    public static final String MOVIE_ID_ACCEPT = ApiEndpoints.QUERY_PARAM_ID + "/accept";
+    public static final String MOVIE_ID_REJECT = ApiEndpoints.QUERY_PARAM_ID + "/reject";
 
     public static final String PARAM_LIMIT = "limit";
     public static final String PARAM_OFFSET = "offset";
